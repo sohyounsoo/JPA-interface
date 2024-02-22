caab756ffd0a7e0db21b5b92f98311fe61aaa2d6
Sohyounsoo <77376245+sohyounsoo@users.noreply.github.com>
2024-02-22 22:29:17 +0900

    Develop (#5)
    * interface
    
    * queryMethod_1
    
    * JPA pagging
    
    * add spring-data-jpa pagingg
    
    * JPA Hint & Lock
    
    ---------
    
    Co-authored-by: so566 <so5663@naver.com>

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"}) // 여기에다가 team을 찍으면 무한루프
+@NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
 public class Member {
 
     @Id @GeneratedValue
@@ -21,6 +22,11 @@ public class Member {
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     public Member(String username, int age, Team team) {
