53bb530d5b9eec47cec82b47ab45a77c327905de
Sohyounsoo <77376245+sohyounsoo@users.noreply.github.com>
2024-03-07 16:20:19 +0900

    feat: Auditin 추가 (#6)
    Co-authored-by: So hyounsoo <so5663@naver.com>

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,12 +2,24 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing // Auditing을 사용하려면 꼭 사용해야 하는 어노테이션 입니다.
 @SpringBootApplication
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
---
+++ src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass //사용하지 않으면 테이블에는 생성이 되지 않음
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createDate = now;
+        updateDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateDate = LocalDateTime.now();
+    }
+}
